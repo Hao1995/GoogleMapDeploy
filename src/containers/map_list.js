fe3dcaad61950c49f991ad327e840a4f297e8c4c
Hao Yu <y26704325@gmail.com>
2017-06-02 02:25:32 +0000

    use 'https' to get geolocation

--- src/containers/map_list.js
+++ src/containers/map_list.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import GoogleMap from "../components/google_map";
 
+var currentLoc,marker,map;
 
 class MapList extends Component {
     renderTest(cityData) {
@@ -11,9 +12,49 @@ class MapList extends Component {
             <GoogleMap lon={lon} lat={lat} />
         );
     }
+    
+    //Current location click event
+    getCurrentLocation() {
+        console.log("getCurrentLocation...");
+        //If brower supports HTML5 geoLocation
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) { 
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            console.log("lat:"+lat+", lng:"+lng);
+            // currentLoc = new google.maps.LatLng(lat, lng);
+    
+            // //Remove previously added marker
+            // if (marker) {
+            //   marker.setMap(null);
+            // }
+    
+            // var popupContent = '<div id="content"><h1 id="firstHeading" class="heading">Your location is found..</h1></div>'
+    
+            // var infowindow = new google.maps.InfoWindow({
+            //   content: popupContent
+            // });
+    
+            // map.setCenter(currentLoc);//Set the map to center of location
+    
+            // marker = new google.maps.Marker({
+            //     map: map,
+            //     zoom: 14,
+            //     position: currentLoc
+            // });
+    
+            // infowindow.open(map,marker);
+          });
+            
+        }
+        else {
+          alert('This Browser doesn\'t support HTML5 geolocation');
+        }
+      }
     render() {
         return (
             <div className="row">
+                <button className="btn btn-default" onClick={this.getCurrentLocation}>Current Location</button>
                 <div id="mapList" className="col-lg-12">
                     {this.props.weather.map(this.renderTest)}
                 </div>

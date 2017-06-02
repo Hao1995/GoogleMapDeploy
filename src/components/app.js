c1c13fbe8425cedf2006ac23484c21c0dff0f2c4
Hao Yu <y26704325@gmail.com>
2017-06-02 02:05:19 +0000

    Q of Middleware

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import SearchBar from "../containers/search_bar";
 import MapList from "../containers/map_list";
+import Location from "../containers/location";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <Location />
         <MapList />
       </div>
     );
--- src/containers/map_list.js
+++ src/containers/map_list.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import Chart from "../components/chart";
 import GoogleMap from "../components/google_map";
 
 
